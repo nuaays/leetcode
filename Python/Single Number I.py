e02532b86b53d7c1a4def7c59b57738df55a0dbc
Yang Sen C <Sen.B.Yang@alcatel-sbell.com.cn>
2014-11-13 15:36:00 +0800

    Signle Number I

---
+++ Python/Single Number I.py
@@ -0,0 +1,25 @@
+import os, sys
+
+"""
+https://oj.leetcode.com/problems/single-number-ii/
+
+Given an array of integers, every element appears three times except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+"""
+
+
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        numhash={}
+        for i in A:
+            if not numhash.has_key(i):
+                numhash[i] = 1
+            else:
+                numhash.pop(i)
+        
+        return numhash.keys()[0]
